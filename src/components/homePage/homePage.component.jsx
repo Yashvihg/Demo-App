4c8d5d4d00820199558d835919e5b390a0b77df0
Nimesh <nimeshjohari95@gmail.com>
2021-07-27 15:33:10 +0530

    Dummy Components

---
+++ src/components/homePage/homePage.component.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+const homePage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+        architecto, provident, voluptatibus praesentium cupiditate porro
+        consequuntur sit dolor distinctio unde sint itaque quam sed veniam
+        excepturi reiciendis, aperiam dolorem. Eos natus, architecto neque
+        nostrum facilis porro, dolore, iure cum harum ratione blanditiis
+        temporibus soluta vero et repellat minus consectetur rem? Deleniti,
+        nulla odio. Possimus beatae maiores rerum, voluptas architecto ipsam.
+        Quas provident cupiditate fugit voluptate tempora repellendus iure
+        assumenda? Ut assumenda exercitationem maxime qui commodi sint dolor
+        voluptatem voluptates, quibusdam ipsam, earum dolorem. Ipsum
+        perspiciatis doloribus rem accusamus temporibus? Amet odit repellendus
+        nesciunt consectetur fugiat. Similique accusamus ad enim at nam
+        blanditiis cumque dolor repellendus quaerat iusto necessitatibus ullam,
+        reiciendis aut aliquid qui perspiciatis amet, obcaecati possimus
+        molestias veniam earum laudantium fuga deserunt consequatur. Beatae
+        officia ea inventore esse, autem laboriosam incidunt quasi tempore. Cum
+        quisquam consectetur aspernatur placeat officia repudiandae nemo minima
+        ab quo molestias? Numquam, magni alias. Amet, dicta perferendis? Tempora
+        id ipsa repellat alias neque laborum enim qui, ab eveniet distinctio
+        cumque nulla recusandae quasi. Ipsam, quisquam expedita asperiores vero
+        commodi sint iste dicta soluta. Dignissimos ducimus excepturi totam,
+        dolorum eaque, consequatur iste qui asperiores unde aperiam quo quaerat
+        saepe iusto neque facilis natus. Similique nostrum atque tenetur amet
+        commodi voluptatum est explicabo consequatur rem autem, veniam magnam.
+        Delectus suscipit, perferendis error consectetur porro fuga illo esse
+        neque recusandae cupiditate id fugit culpa veritatis molestias eius,
+        molestiae deleniti aut nesciunt quis quia laboriosam architecto.
+        Inventore nihil, non, et repellat commodi dicta nisi voluptas autem
+        delectus ipsum sequi sed soluta unde eaque quae ab eos consequatur
+        expedita beatae. Quisquam magni ipsam est, nam quasi voluptatibus
+        corporis illo aut, soluta earum quae harum corrupti sint ad aperiam
+        autem ullam nemo, quia animi beatae culpa. Magnam quia numquam, facere
+        saepe illum sunt eaque inventore rem vitae, officiis error nulla natus
+        dolor accusamus illo minima quae deserunt sed odit quos accusantium
+        autem? Nobis provident beatae dolore amet aperiam. Vel maxime qui at
+        nobis.
+      </p>
+    </div>
+  );
+};
+
+export default homePage;
