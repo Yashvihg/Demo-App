b783e134bfb9c9ce51f14b8ba8d512cde077b8cc
Yashvihg <67501898+Yashvihg@users.noreply.github.com>
2021-07-27 13:53:42 +0400

    components

--- src/components/sidebar/sidebar.component.jsx
+++ src/components/sidebar/sidebar.component.jsx
@@ -15,18 +15,16 @@ const Sidebar = props => {
             >
                 <div className="sidebar-sticky"></div>
             <Nav.Item>
-                <Nav.Link href="/home">Active</Nav.Link>
+                <Nav.Link href="/home">HOME PAGE</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link eventKey="link-1">Link</Nav.Link>
+                <Nav.Link eventKey="link-1">FORM</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link eventKey="link-2">Link</Nav.Link>
+                <Nav.Link eventKey="link-1">ABOUT US</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-                <Nav.Link eventKey="disabled" disabled>
-                Disabled
-                </Nav.Link>
+                <Nav.Link eventKey="link-2">CONTACT US</Nav.Link>
             </Nav.Item>
             </Nav>
           
