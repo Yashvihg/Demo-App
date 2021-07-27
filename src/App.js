aa1fa961b37f0cd94d6df3c7f433fb7785e548a5
Nimesh <nimeshjohari95@gmail.com>
2021-07-27 14:02:37 +0530

    Merged  Form And Sidebar

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React from "react";
 import FormComponent from "./components/form/form.component";
 import Sidebar from "./components/sidebar/sidebar.component";
 function App() {
-  return <Sidebar />;
-
+  return (
+    <div>
+      <Sidebar />
+      <FormComponent />
+    </div>
+  );
 }
 
 export default App;
